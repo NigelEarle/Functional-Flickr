99d6c8c84352bb5d860e26864313707e07e7b3e1
nwre87 <nwre87@gmail.com>
2016-07-19 12:17:23 -1000

    working flickr api call

--- js/flickr.js
+++ js/flickr.js
@@ -10,29 +10,111 @@ require([
     'jquery',
   ],
   function(_, $) {
-    var trace = _.curry(function(tag, x) {
-      console.log("THE TRACE", tag, x);
-      return x;
-    });
 
     var Impure = {
       getJSON : _.curry(function(callback, url){
         $.getJSON(url, callback);
       }),
-      setHTML: _.curry(function(sel, html){
+      setHTML : _.curry(function(sel, html){
         $(sel).html(html);
       })
     };
+
+    var img = function(url){
+      return $('<img />', {
+        src: url
+      });
+    };
+
+    var trace = _.curry(function(tag, x) {
+      console.log("THE TRACE", tag, x);
+      return x;
+    });
 
     var url = function(term) {
       return 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' +
         term + '&format=json&jsoncallback=?';
     };
 
-    var app = _.compose(Impure.getJSON(trace('response')), url);
+
+    var mediaUrl = _.compose(_.prop('m'), _.prop('media'));
+
+    var srcs = _.compose(_.map(mediaUrl), _.prop('items'));
+
+    var images = _.compose(_.map(img), srcs);
+
+    var renderImages = _.compose(Impure.setHTML('body'), images);
+
+    var app = _.compose(Impure.getJSON(renderImages), url);
 
     app('cats');
+
   }
 );
+
+
+
+
+// requirejs.config({
+//   paths: {
+//     ramda: 'https://cdnjs.cloudflare.com/ajax/libs/ramda/0.13.0/ramda.min',
+//     jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+//   },
+// });
+
+// require([
+//     'ramda',
+//     'jquery',
+//   ],
+//   function(_, $) {
+
+//     ////////////////////////////////////////////
+//     // Utils
+
+//     var Impure = {
+//       getJSON: _.curry(function(callback, url) {
+//         $.getJSON(url, callback);
+//       }),
+//       setHtml: _.curry(function(sel, html) {
+//         $(sel).html(html);
+//       }),
+//     };
+
+//     var img = function(url) {
+//       return $('<img />', {
+//         src: url,
+//       });
+//     };
+
+//     var trace = _.curry(function(tag, x) {
+//       console.log(tag, x);
+//       return x;
+//     });
+
+//     var url = function(t) {
+//       return 'http://api.flickr.com/services/feeds/photos_public.gne?tags=' +
+//         t + '&format=json&jsoncallback=?';
+//     };
+
+//     var mediaUrl = _.compose(_.prop('m'), _.prop('media'));
+
+//     var srcs = _.compose(_.map(mediaUrl), _.prop('items'));
+
+//     var images = _.compose(_.map(img), srcs);
+
+//     var renderImages = _.compose(Impure.setHtml('body'), images);
+
+//     var app = _.compose(Impure.getJSON(renderImages), url);
+
+//     app('cats');
+//   }
+// );
+
+
+
+
+
+
+
 
 
